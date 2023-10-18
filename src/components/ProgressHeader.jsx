703a313dabf86ac52d172df68604c7f559bbbb64
Nathan King <nathan@betapoint.co>
2023-10-17 21:02:56 -0400

    notes on css cause this is a cheap way to do it so I need to do it myself.

--- src/components/ProgressHeader.jsx
+++ src/components/ProgressHeader.jsx
@@ -16,21 +16,25 @@ const ProgressHeader = () => {
                 backgroundRepeat: 'no-repeat',
             }}
         >
+            {/* this is hidden if the viewport is over 768 (md:hidden) */}
             <div className="md:hidden bg-[#FFFFFF] text-black outline outline-offset-4 outline-gray-500 rounded-sm cursor-pointer ml-2">
                 {isOpen ? <XLg onClick={() => setIsOpen(!isOpen)} /> : <List onClick={() => setIsOpen(!isOpen)} />}
             </div>
 
             {appData.steps.map((value, index) => {
                 index += 1;
-                // let itemClass = 'flex justify-center items-center p-2 highlight rounded-md bg-[#6C757D] whitespace-no-wrap-text';
+                //set the default class and stepText
                 let itemClass = 'flex justify-center items-center p-2 highlight rounded-md bg-gradient-to-tr from-blue-500 to-purple-500 text-white shadow-lg';
                 let stepText = 'Step ' + index;
-
+                
+                //change the item class and stepText if this is the step we are on
                 if (index == step) {
                     itemClass = 'flex justify-center items-center p-2 highlight flex-grow bg-gradient-to-tr from-blue-500 to-purple-500 text-white shadow-lg ';
                     // itemClass = 'flex justify-center items-center p-2 highlight flex-grow rounded-md bg-[#212529] whitespace-no-wrap-text';
                     stepText += ' - ' + value.title;
                 }
+                
+                //return the div with the item
                 return (
                     <div className={`${isOpen ? 'sm:flex' : 'sm:hidden'} md:flex ${itemClass}`} style={{ height: 30, margin: 10 }} key={index}>
                         <p className="text-[12px] text-white font-bold">{stepText}</p>
